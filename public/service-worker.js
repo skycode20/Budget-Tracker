5259b0958e331e25bf62bfd1d7c23e2682840575
Skyler R <skyler.rencher@gmail.com>
2021-01-12 17:41:06 -0500

    added some variables to the service workerjs.

---
+++ public/service-worker.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = "my-site-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const FILES_TO_CACHE = [
+  //add files to cache
+  "./db.js",
+  "./index.html",
+  "./styles.css",
+  "./index.js",
+  "./icons/icon-192x192.png",
+  "./icons/icon-512x512.png",
+];
+
