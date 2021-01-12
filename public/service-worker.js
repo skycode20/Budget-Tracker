db4c59cf8ac5aaa32614e0765e26f84b52be2bd8
Skyler R <skyler.rencher@gmail.com>
2021-01-12 17:47:40 -0500

    added event listener to open the cache if there is one and to save the cache.

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,12 +2,22 @@ const CACHE_NAME = "my-site-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 const FILES_TO_CACHE = [
-  //add files to cache
-  "./db.js",
-  "./index.html",
-  "./styles.css",
-  "./index.js",
-  "./icons/icon-192x192.png",
-  "./icons/icon-512x512.png",
+    //add files to cache
+    "./db.js",
+    "./index.html",
+    "./styles.css",
+    "./index.js",
+    "./icons/icon-192x192.png",
+    "./icons/icon-512x512.png",
 ];
+
+//service worker
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then((cache) => cache.addAll(FILES_TO_CACHE))
+            .then(self.skipWaiting())
+    );
+});
 
